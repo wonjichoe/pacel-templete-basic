a632af802ea39026028fdee7aedfe131dc590bc6
wonjichoe <wonjichoe@gmail.com>
2024-07-15 13:12:03 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,21 @@
+//ESM
+//CommonJs
+
+// import autoprefixer from 'autoprefixer'
+// const autoprefixer = require('autoprefixer')
+
+// export = {
+//   plugins: [
+//     autoprefixer
+//   ]
+// // }
+// module.exports = {
+//   plugins: [
+//     autoprefixer
+//   ]
+// }
+module.exports = {
+  plugins: [
+    require('autoprefixer')
+  ]
+}
